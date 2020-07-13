70f52f7eede5f0c22f142a164fe356f68282dbf8
collinskoech11 <collinskoechck34@gmail.com>
2020-07-13 13:54:47 +0300

    modal complete

--- js/app.js
+++ js/app.js
@@ -2,3 +2,8 @@ document.getElementById('button').addEventListener('click',
 function() {
 	document.querySelector('.bg-modal').style.display = 'flex';
 });
+
+document.querySelector('.close').addEventListener('click',
+function() {
+	document.querySelector('.bg-modal').style.display = 'none';
+});
